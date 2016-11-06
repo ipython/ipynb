ac2d161771e07aaffdd0d51c5acbb02775a3aa41
YuviPanda <yuvipanda@gmail.com>
2016-11-05 23:22:24 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipynb',
+    version='0.1',
+    description='Package / Module importer for importing code from Jupyter Notebook files (.ipynb)',
+    url='http://github.com/yuvipanda/ipynb',
+    author='Yuvi Panda',
+    author_email='yuvipanda@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+)
