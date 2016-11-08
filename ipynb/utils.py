e0f0e6657ee6b33ec863d943560f78408bc3d5d2
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-11-08 14:10:56 -0800

    Allow to ship packages with ipynb.
    This is just the beginning we should do some normalisation of filenames,
    and figure out how to cleanup the generated .py files.

--- ipynb/utils.py
+++ ipynb/utils.py
@@ -1,21 +1,31 @@
 import json
 
+PREAMBLE=\
+"""
+##############################################################################
+# This source has been auto generated from an IPython/Jupyter notebook file. #
+# Please modify the origin file                                              #
+##############################################################################
+"""
 
-def get_code(data):
+
+def get_code(data, markdown=False):
     """
     Execute contents of a given stream in a given module
 
     data is a file-like object that'll be read from for contents
     module is the module in which to execute this code
 
-    FIXME: If you use any ipython magics here, it'll go boom.
+    FIXME: If you use any IPython magics here, it'll go boom.
     """
     nb = json.loads(data)
 
-    code = ''
+    code = PREAMBLE
     for cell in nb['cells']:
         if cell['cell_type'] == 'code':
             # transform the input to executable Python
             code += '\n'.join(cell['source'])
             code += '\n'
+        if cell['cell_type'] == 'markdown':
+            code += '\n' +'\n# '.join(cell['source'])
     return code
