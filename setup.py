0a4abe2b932e2eb5708e94f7f13313230daa540a
YuviPanda <yuvipanda@gmail.com>
2016-11-07 17:18:12 -0800

    Actually bump version
    We already had a 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipynb',
-    version='0.2',
+    version='0.3',
     description='Package / Module importer for importing code from Jupyter Notebook files (.ipynb)',
     url='http://github.com/yuvipanda/ipynb',
     author='Yuvi Panda',
