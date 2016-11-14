3695f2d8e3a0939cbdab712672a59cc1eb1bb938
YuviPanda <yuvipanda@gmail.com>
2016-11-13 16:46:11 -0800

    Add tests for mixed .ipynb and .py files
    Also refactor tests to have less duplication

--- tests/test_defs.py
+++ tests/test_defs.py
@@ -1,18 +1,26 @@
-import ipynb.fs.defs.pure_ipynb as a
-import ipynb.fs.defs.pure_ipynb.foo as foo
+import pytest
+import importlib
 
 
-def test_a():
-    assert a.init() == 'init'
+@pytest.fixture(
+    scope='module',
+    params=[
+        'ipynb.fs.defs.pure_ipynb.foo',
+        'ipynb.fs.defs.mixed_ipynb.foo'
+    ]
+)
+def mod(request):
+    return importlib.import_module(request.param)
 
-def test_foo():
-    assert foo.foo() == 'foo'
-    rawr = foo.RAWR()
+def test_execute(mod):
+    assert mod.foo() == 'foo'
+    rawr = mod.RAWR()
     assert rawr.rawr() == 'rawr'
 
-def test_no_execute():
-    assert not hasattr(foo, 'bar')
+def test_no_execute(mod):
+    assert not hasattr(mod, 'bar')
+    assert not hasattr(mod, 'r')
 
-def test_allcaps_execute():
-    assert foo.WAT == 'boo'
+def test_allcaps_execute(mod):
+    assert mod.WAT == 'boo'
 
