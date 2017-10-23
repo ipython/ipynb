2f1526a447104d7d7b97e2a8ab66bee8d2da90ad
yuvipanda <yuvipanda@gmail.com>
2017-10-22 23:20:16 -0700

    Bump version to v0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipynb',
-    version='0.5',
+    version='0.5.1',
     description='Package / Module importer for importing code from Jupyter Notebook files (.ipynb)',
     url='http://github.com/yuvipanda/ipynb',
     author='Yuvi Panda',
