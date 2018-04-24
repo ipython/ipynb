ef8d85cb36b011268dc0c2430270cf9b2bbefc88
tonyfast <tony.fast@gmail.com>
2018-04-24 09:54:45 -0400

    Remove the tests from source code.

--- importnb/__init__.py
+++ importnb/__init__.py
@@ -56,13 +56,20 @@ class Notebook(SourceFileLoader, ImportContextMixin):
     """A SourceFileLoader for notebooks that provides line number debugginer in the JSON source."""
     EXTENSION_SUFFIXES = '.ipynb',
     
-    def __init__(self, fullname=None, path=None): super().__init__(fullname, path)
+    def __init__(self, fullname=None, path=None, *,capture=True): 
+        self.capture = capture
+        super().__init__(fullname, path)
     
     def exec_module(Loader, module):
+        module.__output__ = None
+        if Loader.capture: return Loader.exec_module_capture(module)
+        else: return super().exec_module(module)
+        
+    def exec_module_capture(Loader, module):
         from IPython.utils.capture import capture_output    
         with capture_output(stdout=False, stderr=False) as output: 
             try: super().exec_module(module)
-            except: ...
+            except type('pass', (BaseException,), {}): ...
             finally: module.__output__ = output
         return module
 
@@ -82,7 +89,7 @@ class Partial(Notebook):
                 name=module.__name__, file=module.__file__
             ))
             except ImportWarning as error:
-                print_exc()
+                if not loader.capture: print_exc()
                 module.__exception__ = exception
         return module
 
@@ -96,38 +103,11 @@ def load_ipython_extension(ip=None): Notebook().__enter__()
 def unload_ipython_extension(ip=None): Notebook().__exit__()
 
 
+# # Developer
+
 # In[7]:
 
 
-class Test(__import__('unittest').TestCase): 
-    def setUp(Test):
-        from nbformat import write, v4
-        load_ipython_extension()
-        with open('test_loader.ipynb', 'w') as file:
-            write(v4.new_notebook(cells=[
-                v4.new_code_cell("""__import__("time").sleep(1);test = 42""")
-            ]), file)
-            
-    def runTest(Test):
-        import test_loader
-        assert test_loader.__file__.endswith('.ipynb')
-        assert test_loader.test is 42
-        assert isinstance(test_loader, __import__('types').ModuleType)
-        
-    def tearDown(Test):
-#             %rm test_loader.ipynb
-        unload_ipython_extension()
-
-
-# # Developer
-
-# In[8]:
-
-
 if __name__ ==  '__main__':
-#         __import__('doctest').testmod(verbose=2)
-#         __import__('unittest').TextTestRunner().run(Test())
     get_ipython().system('jupyter nbconvert --to script --TemplateExporter.exclude_input_prompt=True __init__.ipynb')
-    with Notebook():
-        import test_failure
 
