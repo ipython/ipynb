d851e8742efed92b5c9f4b6508accb2a3261a207
Tony Fast <tony.fast@gmail.com>
2018-04-27 12:05:17 -0400

    Use Pytest on travis instead of unittest (#16)
    * Prepare doctests in the source notebooks to test with pytest
    
    * Add pytest requirements and execution directions
    
    * Change the python pytest execution
    
    * Skip the ipython compiler in the python pytest
    
    * Someone might assume i have no idea what I'm doing.  Thats true
    
    * Testing is hard, but it only needs to be done once.
    
    * 🌭worst🌭
    
    * Revert "Change the python pytest execution"
    
    This reverts commit 38fa806160c3061dd9fc5dbf5316fe69ff2d888f.
    
    * Use the command line to skip tests.
    
    * This will all be worth it 🤞

--- importnb/exporter.py
+++ importnb/exporter.py
@@ -1,18 +1,36 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 try:
     from .decoder import LineNoDecoder
 except:
-    from decoder import LineNoDecoder
+    from decoder import LineNoDecoder        
+    
+__file__ = globals().get('__file__', 'exporter.ipynb')
+
+
+# # Compilation
+# 
+# Compilation occurs in the __3__ steps:
+# 
+# 1. Text is transformed into a valid source string.
+# 2. The source string is parsed into an abstract syntax tree
+# 3. The abstract syntax compiles to valid bytecode 
+
+# In[2]:
+
 
 __IPYTHON__ = False
 try:
     from IPython import get_ipython
-
     if not get_ipython():
         raise ValueError("""There is no interactive IPython shell""")
     __IPYTHON__ = True
-except:
-    ...
-
+except: ...
+    
 if __IPYTHON__:
     try:
         from .compiler_ipython import Compiler, NotebookNode, NotebookExporter
@@ -23,6 +41,10 @@ else:
         from .compiler_python import Compiler, NotebookNode, NotebookExporter
     except:
         from compiler_python import Compiler, NotebookNode, NotebookExporter
+        
+
+
+# In[3]:
 
 
 import ast, sys
@@ -31,76 +53,71 @@ from pathlib import Path
 from dataclasses import dataclass
 
 
+# In[4]:
+
+
 @dataclass
 class Code(NotebookExporter, Compiler):
     """An exporter than returns transforms a NotebookNode through the InputSplitter.
-
-    >>> assert type(Code().from_filename('exporter.ipynb')) is NotebookNode"""
-    filename: str = "<module exporter>"
-    name: str = "__main__"
+    
+    >>> assert type(Code().from_filename(Path(__file__).with_suffix('.ipynb'))) is NotebookNode"""
+    filename: str = '<module exporter>'
+    name: str = '__main__'
     decoder: type = LineNoDecoder
-
-    def __post_init__(self):
-        NotebookExporter.__init__(self) or Compiler.__init__(self)
-
-    def from_file(Code, file_stream, resources=None, **dict):
-        for str in ("name", "filename"):
-            setattr(Code, str, dict.pop(str, getattr(Code, str)))
+        
+    def __post_init__(self): NotebookExporter.__init__(self) or Compiler.__init__(self)
+            
+    def from_file(Code,file_stream, resources=None, **dict): 
+        for str in ('name', 'filename'): setattr(Code, str, dict.pop(str, getattr(Code, str)))
         return Code.from_notebook_node(
-            NotebookNode(**load(file_stream, cls=Code.decoder)), resources, **dict
-        )
-
-    def from_filename(Code, filename, resources=None, **dict):
+            NotebookNode(**load(file_stream, cls=Code.decoder)), resources, **dict)
+    
+    def from_filename(Code,  filename, resources=None, **dict):
         Code.filename, Code.name = filename, Path(filename).stem
         return super().from_filename(filename, resources, **dict)
 
-    def from_notebook_node(Code, nb, resources=None, **dict):
-        for cell in nb["cells"]:
-            if cell["cell_type"] == "code":
-                cell["source"] = Code.from_code_cell(cell, **dict)
+    def from_notebook_node(Code, nb, resources=None, **dict): 
+        for cell in nb['cells']:
+            if cell['cell_type'] == 'code':
+                cell['source'] = Code.from_code_cell(cell, **dict)
         return nb
+    
+    def from_code_cell(Code, cell, **dict):  
+        return Code.transform(cell['source'])
 
-    def from_code_cell(Code, cell, **dict):
-        return Code.transform(cell["source"])
+
+# In[5]:
 
 
 class AST(Code):
     """An exporter than returns parsed ast.
+    
+    >>> assert type(AST().from_filename(Path(__file__).with_suffix('.ipynb'))) is ast.Module"""
+    def from_notebook_node(AST, nb: NotebookNode, resource: dict=None, **dict):         
+        return AST.ast_transform(ast.fix_missing_locations(ast.Module(body=sum((
+            AST.ast_parse(
+                AST.from_code_cell(cell, **dict), lineno=cell['metadata'].get('lineno', 1)
+            ).body for cell in nb['cells'] if cell['cell_type']=='code'
+        ), []))))
 
-    >>> assert type(AST().from_filename('exporter.ipynb')) is ast.Module"""
 
-    def from_notebook_node(AST, nb: NotebookNode, resource: dict = None, **dict):
-        return AST.ast_transform(
-            ast.fix_missing_locations(
-                ast.Module(
-                    body=sum(
-                        (
-                            AST.ast_parse(
-                                AST.from_code_cell(cell, **dict),
-                                lineno=cell["metadata"].get("lineno", 1),
-                            ).body
-                            for cell in nb["cells"]
-                            if cell["cell_type"] == "code"
-                        ),
-                        [],
-                    )
-                )
-            )
-        )
+# In[6]:
 
 
 class Compile(AST):
     """An exporter that returns compiled and cached bytecode.
-
-    >>> assert Compile().from_filename('exporter.ipynb')"""
-
-    def from_notebook_node(Compile, nb, resources: dict = None, **dict):
+    
+    >>> assert Compile().from_filename(Path(__file__).with_suffix('.ipynb'))"""        
+    def from_notebook_node(Compile, nb, resources: dict=None, **dict):
         return Compile.compile(super().from_notebook_node(nb, resources, **dict))
 
 
-if __name__ == "__main__":
+# In[7]:
+
+
+if __name__ ==  '__main__':
     from pathlib import Path
     from nbconvert.exporters.script import ScriptExporter
+    Path('exporter.py').write_text(ScriptExporter().from_filename('exporter.ipynb')[0])
+    __import__('doctest').testmod()
 
-    Path("exporter.py").write_text(ScriptExporter().from_filename("exporter.ipynb")[0])
-    __import__("doctest").testmod()
