0185e397470387d073fedde1cf373ae20ad7f909
tonyfast <tony.fast@gmail.com>
2018-05-27 23:17:39 -0400

    Deprecate parameterize for execute

--- src/importnb/execute.py
+++ src/importnb/execute.py
@@ -1,15 +1,35 @@
 # coding: utf-8
+'''# The `Execute` importer
+
+The execute importer maintains an attribute that includes the notebooks inputs and outputs.
+
+    >>> import importnb    
+    >>> from importnb import notebooks
+    >>> with Execute(stdout=True):
+    ...      from importnb.notebooks import execute as nb
+    
+An executed notebook contains a `__notebook__` attributes that is populated with cell outputs.
+
+    >>> assert nb._notebook
+    
+The `__notebook__` attribute complies with `nbformat`
+
+    >>> from nbformat.v4 import new_notebook
+    >>> assert new_notebook(**nb._notebook), """The notebook is not a valid nbformat"""
+    
+'''
+
 if globals().get("show", None):
     print("I am tested.")
 
 try:
     from .capture import capture_output
-    from .loader import Notebook, advanced_exec_module, markdown_to_source
-    from .decoder import loads_ast, identity, loads, dedent, cell_to_ast
+    from .loader import Notebook, advanced_exec_module
+    from .decoder import identity, loads, dedent
 except:
     from capture import capture_output
-    from loader import Notebook, advanced_exec_module, markdown_to_source
-    from decoder import loads_ast, identity, loads, dedent, cell_to_ast
+    from loader import Notebook, advanced_exec_module
+    from decoder import identity, loads, dedent
 
 import inspect, sys, ast
 from functools import partialmethod, partial
@@ -36,8 +56,14 @@ from collections import ChainMap
 
 __all__ = "Notebook", "Partial", "reload", "Lazy"
 
+"""# Loaders that reproduce notebook outputs
+"""
+
+
 def loader_include_notebook(loader, module):
-    module._notebook = loads(loader.get_data(loader.path).decode("utf-8"))
+    if not hasattr(module, "_notebook"):
+        module._notebook = loads(loader.get_data(loader.path).decode("utf-8"))
+
 
 class NotebookCells(Notebook):
     """The NotebookCells loader's contain a _notebook attributes containing a state of the notebook.
@@ -51,6 +77,14 @@ class NotebookCells(Notebook):
         _call_with_frames_removed(
             exec, self.source_to_code(module._notebook), module.__dict__, module.__dict__
         )
+
+
+if __name__ == "__main__":
+    nb = NotebookCells().from_filename("execute.ipynb", "importnb.notebooks")
+
+"""## Recreating IPython output objectss
+"""
+
 
 def new_stream(text, name="stdout"):
     return {"name": name, "output_type": "stream", "text": text}
@@ -67,6 +101,11 @@ def new_error(Exception):
 
 def new_display(object):
     return {"data": object.data, "metadata": {}, "output_type": "display_data"}
+
+
+"""# Reproduce notebooks with the `Execute` class.
+"""
+
 
 class Execute(Notebook):
     """The Execute loader reproduces outputs in the module._notebook attribute.
@@ -96,13 +135,12 @@ class Execute(Notebook):
             error = None
             with capture_output() as out:
                 try:
-                    if i == 0:
-                        cell = markdown_to_source(cell)
-                    if cell["cell_type"] == "code":
-
-                        _call_with_frames_removed(
-                            exec, self.source_to_code(cell), module.__dict__, module.__dict__
-                        )
+                    _call_with_frames_removed(
+                        exec,
+                        self.source_to_code(cell, interactive=bool(i)),
+                        module.__dict__,
+                        module.__dict__,
+                    )
                 except BaseException as Exception:
                     error = new_error(Exception)
                     try:
@@ -112,54 +150,83 @@ class Execute(Notebook):
                         ...
                     break
                 finally:
-                    if out.outputs:
-                        cell["outputs"] += [new_display(object) for object in out.outputs]
-                    if out.stdout:
-                        cell["outputs"] += [new_stream(out.stdout)]
-                    if error:
-                        cell["outputs"] += [error]
-                    if out.stderr:
-                        cell["outputs"] += [new_stream(out.stderr, "stderr")]
+                    if "outputs" in cell:
+                        if out.outputs:
+                            cell["outputs"] += [new_display(object) for object in out.outputs]
+                        if out.stdout:
+                            cell["outputs"] += [new_stream(out.stdout)]
+                        if error:
+                            cell["outputs"] += [error]
+                        if out.stderr:
+                            cell["outputs"] += [new_stream(out.stderr, "stderr")]
             out.show()
 
-    source_to_code = partialmethod(NotebookCells.source_to_code)
+    def source_to_code(loader, object, path=None, interactive=True):
+        """Transform ast modules into Interactive and Expression nodes. This 
+        will allow the cell outputs to be captured.  `interactive` is only true for
+        the first markdown cell.
+        """
+
+        node = loader.visit(object)
+
+        if isinstance(node, ast.Expr):
+            """An expression is a special case where a markdown cell was 
+            turned into a docstring
+            """
+            if interactive:
+                """An expression will not print to the stdout."""
+                node = ast.Expression(body=node.value)
+                mode = "eval"
+            else:
+                """This tree replaces the docstring"""
+                node = ast.Module(body=[node])
+                mode = "exec"
+        elif isinstance(node, ast.Module):
+            """In the exectuion mode we use interactive nodes to capture stdouts."""
+            node = ast.Interactive(body=node.body)
+            mode = "single"
+        return compile(node, path or "<importnb>", mode)
+
 
 if __name__ == "__main__":
-    nb = Execute(display=True, stdout=True).from_filename("execute.ipynb", "importnb.notebooks")
+    nb = Execute().from_filename("execute.ipynb", "importnb.notebooks")
 
-class ParameterizeNode(NodeTransformer):
-    """Discover any literal ast expression and create parameters from them. 
-    
-    >>> assert len(ParameterizeNode().visit(ast.parse('''
-    ... foo = 42
-    ... bar = foo''')).body) ==1
-    """
-    visit_Module = NodeTransformer.generic_visit
+"""# Parameterizing notebooks
+"""
 
-    def visit_Assign(FreeStatement, node):
+
+class AssignmentFinder(NodeTransformer):
+    visit_Interactive = visit_Module = NodeTransformer.generic_visit
+
+    def visit_Assign(self, node):
         if len(node.targets):
             try:
                 if not getattr(node.targets[0], "id", "_").startswith("_"):
                     literal_eval(node.value)
                     return node
             except:
-                assert True, """The target can not will not literally evaluate."""
-        return None
+                ...
 
     def generic_visit(self, node):
         ...
 
-def vars_to_sig(**vars):
-    """Create a signature for a dictionary of names."""
-    from inspect import Parameter, Signature
 
-    return Signature([Parameter(str, Parameter.KEYWORD_ONLY, default=vars[str]) for str in vars])
+class AssignmentIgnore(AssignmentFinder):
 
-class Parameterize(Execute, ParameterizeNode):
+    def visit_Assign(self, node):
+        if isinstance(super().visit_Assign(node), ast.Assign):
+            return ast.Expr(ast.NameConstant(value=None))
+        return node
+
+    generic_visit = NodeTransformer.generic_visit
+
+
+class Parameterize(Execute):
     """Discover any literal ast expression and create parameters from them. 
     
-    >>> nb = Parameterize().from_filename('execute.ipynb', 'importnb.notebooks')
-    >>> assert 'a_variable_to_parameterize' in nb.__signature__.parameters
+    >>> f = Parameterize().from_filename('execute.ipynb', 'importnb.notebooks')
+    >>> assert 'a_variable_to_parameterize' in f.__signature__.parameters
+    >>> assert f(a_variable_to_parameterize=100).a_variable_to_parameterize == 100
     
     Parametize is a NodeTransformer that import any nodes return by Parameterize Node.
     
@@ -168,32 +235,23 @@ class Parameterize(Execute, ParameterizeNode):
     ... bar = foo''')).body) ==2
     """
 
-    def visit_Assign(self, node):
-        if super().visit_Assign(node):
-            return ast.Expr(Ellipsis())
-        return node
-
-    def generic_visit(self, node):
-        return node
-
     def create_module(self, spec):
         module = super().create_module(spec)
 
         # Import the notebook when parameterize is imported
         loader_include_notebook(self, module)
 
-        nodes = self.nb_to_ast(module._notebook)
+        node = Notebook().visit(module._notebook)
 
         # Extra effort to supply a docstring
         doc = None
-        if isinstance(nodes, ast.Module) and nodes.body:
-            node = nodes.body[0]
-            if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
-                doc = node
+        if isinstance(node, ast.Module) and node.body:
+            _node = node.body[0]
+            if isinstance(_node, ast.Expr) and isinstance(_node.value, ast.Str):
+                doc = _node
 
-        # Discover the parameterizable nodes
-        params = ParameterizeNode().visit(nodes)
-
+            # Discover the parameterizable nodes
+        params = AssignmentFinder().visit(node)
         # Include the string in the compilation
         doc and params.body.insert(0, doc)
 
@@ -215,7 +273,25 @@ class Parameterize(Execute, ParameterizeNode):
         recall.__doc__ = module.__doc__
         return recall
 
+    def visit(self, node):
+        return AssignmentIgnore().visit(super().visit(node))
+
+
+def vars_to_sig(**vars):
+    """Create a signature for a dictionary of names."""
+    from inspect import Parameter, Signature
+
+    return Signature([Parameter(str, Parameter.KEYWORD_ONLY, default=vars[str]) for str in vars])
+
+
+if __name__ == "__main__":
+    f = Parameterize(display=True).from_filename("execute.ipynb", "importnb.notebooks")
+    print(f(a_variable_to_parameterize=1000).a_variable_to_parameterize)
+
 a_variable_to_parameterize = 42
+
+"""# Developer
+"""
 
 if __name__ == "__main__":
     try:
@@ -226,3 +302,5 @@ if __name__ == "__main__":
     module = Execute().from_filename("execute.ipynb")
     __import__("doctest").testmod(module, verbose=2)
 
+"""How do the interactive nodes work?
+"""
