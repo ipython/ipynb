7bec32d4de27c1d66d523081762d0abd01005d78
Tony Fast <tony.fast@gmail.com>
2018-10-19 14:04:11 -0400

    Ipython profile creates a profile it doesn't exist. (#74)
    * Create an ipython profile if it doesnt exist.
    * Use ipython internals to locate the profile
    * Make the dirs if they don't exist

--- src/importnb/utils/ipython.py
+++ src/importnb/utils/ipython.py
@@ -1,13 +1,19 @@
 # coding: utf-8
 from IPython import paths, get_ipython
-
+from IPython.core import profiledir
 from pathlib import Path
 import json, ast
+import os
 
 
-def get_config():
-    ip = get_ipython()
-    return Path(ip.profile_dir.location if ip else paths.locate_profile()) / "ipython_config.json"
+def get_config(profile="default"):
+    profile_dir = profiledir.ProfileDir()
+    try:
+        profile = profile_dir.find_profile_dir_by_name(paths.get_ipython_dir(), profile)
+    except profiledir.ProfileDirError:
+        os.makedirs(paths.get_ipython_dir(), exist_ok=True)
+        profile = profile_dir.create_profile_dir_by_name(paths.get_ipython_dir(), profile)
+    return Path(profile.location, "ipython_config.json")
 
 
 def load_config():
