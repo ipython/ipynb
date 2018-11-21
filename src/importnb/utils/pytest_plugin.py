212edaf77d6cf13413a1b94a73483ece8b265ae2
Tony Fast <tony.fast@gmail.com>
2018-11-21 18:49:12 -0500

    Pytest subclass (#79)
    * New plugin approach
    * Update _version.py

--- src/importnb/utils/pytest_plugin.py
+++ src/importnb/utils/pytest_plugin.py
@@ -1,13 +1,7 @@
 # coding: utf-8
-import pytest
+import importlib, pytest, abc, pathlib
 
-try:
-    from .. import Notebook
-except:
-    from importnb import Notebook
-from pathlib import Path
-
-loader = Notebook
+from importnb import Notebook
 
 
 def pytest_addoption(parser):
@@ -15,23 +9,35 @@ def pytest_addoption(parser):
     group.addoption("--main", action="store_true", help="Run in the main context.")
 
 
-def pytest_collect_file(parent, path):
-    if "".join(Path(str(path)).suffixes) in (".ipynb",):
-        if not parent.session.isinitpath(path):
-            for pat in parent.config.getini("python_files"):
-                if path.fnmatch(pat.rstrip(".py") + path.ext):
-                    break
-            else:
-                return
-        return PytestModule(path, parent)
+class AlternativeModule(pytest.Module):
+    def _getobj(self):
+        return self.loader(self.parent.config.option.main and "__main__" or None).load(
+            str(self.fspath)
+        )
 
 
-class PytestModule(pytest.Module):
-    def collect(self):
-        global loader
-        with loader(self.parent.config.option.main and "__main__" or None):
-            return super().collect()
+class NotebookModule(AlternativeModule):
+    loader = Notebook
 
+
+class AlternativeSourceText(abc.ABCMeta):
+    def __call__(self, parent, path):
+        for module in self.modules:
+            if "".join(pathlib.Path(str(path)).suffixes) in module.loader.extensions:
+                if not parent.session.isinitpath(path):
+                    for pat in parent.config.getini("python_files"):
+                        if path.fnmatch(pat.rstrip(".py") + path.ext):
+                            break
+                    else:
+                        return
+                return module(path, parent)
+
+
+class NotebookTests(metaclass=AlternativeSourceText):
+    modules = (NotebookModule,)
+
+
+pytest_collect_file = NotebookTests.__call__
 
 if __name__ == "__main__":
     from importnb.utils.export import export
