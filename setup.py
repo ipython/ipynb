aa5fc3f4207c2e55f2acb24ea354b6a0742aeabb
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-12-05 22:58:52 -0500

    remove importnb-run entry_point (#88)

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ setup_args = dict(
         'console_scripts': [
             'importnb-install = importnb.utils.ipython:install',
             'importnb-uninstall = importnb.utils.ipython:uninstall',
-            'importnb-run = importnb.loader:main',
             'nbdoctest = importnb.utils.nbdoctest:_test',
         ]
     },
