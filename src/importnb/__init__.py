700132c558f6b8817fd3beab6feb104e75879ce5
Tony Fast <tony.fast@gmail.com>
2020-01-22 12:37:48 -0500

    Update __init__.py

--- src/importnb/__init__.py
+++ src/importnb/__init__.py
@@ -6,3 +6,4 @@ from ._version import *
 from .ipython_extension import load_ipython_extension, unload_ipython_extension
 from .loader import Notebook, reload, unload_ipython_extension
 from .remote import Remote
+from . import utils
